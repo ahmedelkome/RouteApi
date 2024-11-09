b5b9a7e882c8d41a2719427f0a286d2a2ce4a8a3
ahmedelkomy <ahmedelkomy.2889@gmail.com>
2024-11-09 21:33:59 +0200

    Add project files.

---
+++ HttpClient/Controllers/EmployeeController.cs
@@ -0,0 +1,47 @@
+﻿using EmployeeDetailsEntity.Models.emp;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+    
+
+        [ApiController]
+        [Route("EmployeeDetailsApiRedirect")]
+        public class EmployeeController : ControllerBase
+        {
+            private readonly HttpClient _httpClient;
+
+            public EmployeeController(HttpClient httpClient)
+            {
+                _httpClient = httpClient;
+            }
+
+            [HttpGet("GetAllEmployee")]
+            public async Task<IActionResult> GetAllEmployee()
+            {
+                try
+                {
+                    var getResultApi = await _httpClient.GetAsync("https://localhost:44317/EmployeeDetailsApi/GetAllEmployee");
+
+                    if (getResultApi.IsSuccessStatusCode)
+                    {
+                        var result = await getResultApi.Content.ReadAsStringAsync();
+                        var employeeList = JsonConvert.DeserializeObject<List<Employee>>(result);
+                        return Ok(employeeList);
+                    }
+                    else
+                    {
+
+                        return BadRequest("Error: " + getResultApi.ReasonPhrase);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(500, $"Internal server error: {ex.Message}");
+                }
+            }
+
+
+        }
+    
+
+
---
+++ HttpClient/Models/Employee.cs
@@ -0,0 +1,62 @@
+﻿
+
+using System.Text.Json.Serialization;
+
+namespace EmployeeDetailsEntity.Models.emp
+{
+    public  class Employee
+    {
+        [JsonIgnore]
+        public int id { get; set; } 
+
+        public string? fName { get; set; } = "";
+
+        public string? lName { get; set; } = "";
+
+        public int? age { get; set; } = 0;
+
+        public string? email { get; set; } = "";
+
+        public decimal? salary { get; set; } = 0;
+
+        public string? phone { get; set; } = "";
+
+        public string? country { get; set; } = "";
+
+        public string? state { get; set; } = "";
+
+        public string? gender { get; set; } = "";
+
+        public List<EmployeeDetails> EmployeeDetails { get; set; }
+    }
+
+    public class EmployeeDetails
+    {
+        [JsonIgnore]
+        public int id { get; set; }
+
+        public string? city { get; set; } = "";
+
+        public int? experience { get; set; } = 0;
+
+        public string? jobTitle { get; set; } = "";
+
+        [JsonIgnore]
+        public int? Employeeid { get; set; } = 0;
+
+    }
+
+    public class EmployeeDto
+    {
+        public int id { get; set; }
+
+        public string? city { get; set; }
+
+        public int? experience { get; set; }
+
+        public string? jobTitle { get; set; }
+
+        public string? Employeename { get; set; }
+
+    }
+}
